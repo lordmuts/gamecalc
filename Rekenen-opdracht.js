06682d41c539ee6fe459711fb04aebb2e0c386c7
JPvanGeel <jan.pieter@hotmail.nl>
2019-02-15 15:46:08 +0100

    Balloon v2

--- Rekenen-opdracht.js
+++ Rekenen-opdracht.js
@@ -1,133 +1,102 @@
-$(document).ready(() => {
-    var first;
-    var second;
-    var outputAnswer;    
-    var inputAnswer;
-    var amounts = 0;
-    var timer = 10000;
-    var counter = 10;
-    var balloon = $("#balloon");
+var first;
+var second;
+var outputAnswer;    
+var inputAnswer;
+var amounts = 0;
+var timer = 10000;
+var counter = 10;
+var newHeight = (window.innerHeight - 85);
+var balloonDown = -200
+var operatorArray= ["/","+", "-", "*"];
+var balloon1 = $("#balloon");
+var topBalloon = 0
+var balloonSpeed = 100
 
-    var speed=5;
-    // generates two random numbers and add them to the dom (Todo: pick operator)
-    function makeSum() {
-        first = 1 + Math.floor(Math.random() * 9);
-        second = 1 + Math.floor(Math.random() * 9);
 
-        var operatorArray= ["/","+", "-", "*"];
-        var numberOperator = operatorArray [Math.floor(Math.random() * operatorArray.length)];
+// balloon
+var inputAnswer = false;
+// function balloonStart () {
+//     $(".balloon").animate({
+//         top: "+=50px"
+//     }, "slow" )
 
-        while(numberOperator === "-" && first < second){
-            first++;
-        }
-        while(numberOperator === "/" && first % second != 0) {
-            first++;
-        }
+// }
 
-        $("#firstsum").text(first) 
-        $("#secondsum").text(second)
-        $("#operator").text(numberOperator);
-    };
 
-    var timer = setInterval(() => {
-        counter--;
-        $("#time").html(counter);
-        if(counter < 0 ){
-          //  alert("Game over")
-            console.log("Game over")
-            clearInterval(timer)
-        }
-        myMove();
-       
-    }, 1000);
-    
-    // setInterval(function(){ 
-    //    alert("Game-Over"); }, timer)
 
-    //     clearInterval(speed);
+function makeSum() {
+    first = 1 + Math.floor(Math.random() * 9);
+    second = 1 + Math.floor(Math.random() * 9);
+    console.log({first, second})
 
-    function checkSum() {
-        inputAnswer = eval($("#answer").val())
-        operator = $("#operator").text()
+    // var operatorArray= ["/","+", "-", "*"];
+    var numberOperator = operatorArray [Math.floor(Math.random() * operatorArray.length)];
+    console.log(numberOperator)
 
-        if(operator === "*"){
-            outputAnswer = (first * second);
-        } else if( operator === "/") {
-            outputAnswer = (first / second);
-        } else if( operator === "-") {
-            outputAnswer = (first - second);
-        }
-          else {
-            outputAnswer = (first + second);
-          }
-        if(inputAnswer === outputAnswer) {
-            return true
-        } 
-        else {
-            return false
-        }  
+    while(numberOperator === "-" && first < second){
+        first++;
     }
-    function myMove() {
-        var elem = $("#balloon");
-        var pos = 0;
-        var idPosition = setInterval(frame, 10);
-        function frame() {
-            if (pos == 350) {
-                clearInterval(idPosition);
-            } else {
-                pos++
-                elem.style.top = pos + "px";
-            }
-        }
+    while(numberOperator === "/" && first % second != 0) {
+        first++;
+    }
+    $("#firstsum").text(first) 
+    $("#secondsum").text(second)
+    $("#operator").text(numberOperator);
+};
+
+
+
+function checkSum() {
+    inputAnswer = eval($("#answer").val())
+    operator = $("#operator").text();
+    if(operator === "*"){
+        outputAnswer = (first * second);
+    } else if( operator === "/") {
+        outputAnswer = (first / second);
         
-
-
-        elem.css('top', parseInt(elem.css(window.height/10)))
-        // var elem1 = elem.css('top', parseInt(elem.css('top')) + 10 +'%'); 
-            
-        // debugger
-        //  var pos = elem.position().top;
+    } else if( operator === "-") {
+        outputAnswer = (first - second);
         
-        // var id = setInterval(frame,timer);
-        // function frame() {
-        //   if (pos == 1000) {
-        //     clearInterval(id);
-        //   } else {
-        //     elem.css({"top": pos- speed })
-            
+    }
+    else {
+        outputAnswer = (first + second);
+    }
+}
 
-        //   }
-        }
-      
-     
-
-
-
-    makeSum();
-    // myMove();
-  // timer with setInterval. Decrement seconds every 1000ms 
-    document.addEventListener('keyup', function(e){
+function giveAnswer() {
+    document.getElementById("answer").addEventListener('keyup', function(e) {
         if(e.which == 13) {
-            //event.preventDefault()
-           // console.log(setInterval);
-            if(  checkSum()){
-                amounts++
-                console.log("correct answer")
-                counter +=100;
-                // myMove();
-                // timer += 1000;
-            } 
-            else {
-                counter -= 2;
-            }
-            
-            inputAnswer = eval($("#answer").val(""))
-            $("#counter").text(amounts);
-            makeSum();
-    
-        }
-        
-    
-    })
+            checkSum()
+            if(inputAnswer == outputAnswer) { 
+                console.log("correct")
+                $("#youranswer").text("that is correct")
+                topBalloon -= balloonSpeed
+                counter +=2;
+            } else if (inputAnswer !== outputAnswer) {
+                console.log("not correct 2")
+                $("#youranswer").text("not correct")
 
+                topBalloon += balloonSpeed
+                counter -=4;
+            }
+            makeSum()
+        }
+    })
+}
+
+$(document).ready(() => {
+    $("#start").click(function(){
+        var timer = setInterval(() => {
+            counter--;
+            balloon1.css({top: (topBalloon+= balloonSpeed/5) + "px"})
+            $("#time").html(counter)
+            if(counter <= 0 ){
+                  console.log("Game over")
+                  $("#youranswer").text("game over")
+                  clearInterval(timer)
+            }
+        }, 1000)
+        makeSum()
+    })
+    giveAnswer()
 })
